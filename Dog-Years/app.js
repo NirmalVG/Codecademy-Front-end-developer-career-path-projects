f1d19f3e284d8b568b92f68c941c1208920b2636
NIRMAL V G <38987580+NirmalVG@users.noreply.github.com>
2021-06-10 07:50:53 +0200

    Commit changes

---
+++ Dog-Years/app.js
@@ -0,0 +1,15 @@
+//My age
+const myAge = 23;
+//Early years is 2
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+//Calculate the number of dog years for later years
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+//Add earlyYears and laterYears together, and store that in a variable named myAgeInDogYears
+myAgeInDogYears = earlyYears + laterYears;
+const myName = 'Nirmal'.toLowerCase();
+//Display name and age in dog years
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} old in dog years.`);
