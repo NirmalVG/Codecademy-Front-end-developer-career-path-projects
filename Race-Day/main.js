25ae163731cb8d55e815f2a0956e2c18d7cdb5aa
NIRMAL V G <38987580+NirmalVG@users.noreply.github.com>
2021-06-11 17:35:12 +0200

    Commit changes

---
+++ Race-Day/main.js
@@ -0,0 +1,17 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+let registeredEarly = false;
+let age = 23;
+
+if (registeredEarly && age > 18) {
+  raceNumber += 1000;
+}
+
+if (registeredEarly && age > 18) {
+  console.log(`Race will begin at 9:30, your race number is: ${raceNumber}.`);
+} else if (!registeredEarly && age > 18) {
+  console.log(`Race will begin at 11:00, your race number is: ${raceNumber}.`);
+} else if (age < 18) {
+  console.log(`Race will begin at 12:30, your race number is: ${raceNumber}.`);
+} else {
+  console.log('Please approach the registration desk, thanks!');
+}
